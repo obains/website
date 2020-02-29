aee1d49ce774735b701b6418d50184b5832126f0
Oliver Bains <oliverbains@Olivers-MacBook-Pro.local>
2020-02-29 17:20:13 +0100

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -127,28 +127,9 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
-            <ul className="actions">
-              <li>
-                <input type="submit" value="Send Message" className="special" />
-              </li>
-              <li>
-                <input type="reset" value="Reset" />
-              </li>
-            </ul>
-          </form>
+          <p>You can get in contact with me either through <a href="mailto:oliverbains&#64;gmx&period;de?Subject=Website%20request"> email </a>
+          or through the below icons.
+          </p>
           <ul className="icons">
             <li>
               <a href="https://www.linkedin.com/in/oliver-bains/" className="icon fa-linkedin">
