9fded9c052276eae0abc0df39b0fdd8182844ce1
Oliver Bains <oliverbains@Olivers-MacBook-Pro.local>
2020-02-29 18:15:01 +0100

    new background

--- src/components/Main.js
+++ src/components/Main.js
@@ -115,6 +115,10 @@ class Main extends React.Component {
           <span className="image main">
             <img src={pic06} alt="" />
           </span>
+          <h3>This website</h3>
+          <p>
+            This website was created using Gatsby.JS.
+          </p>
           {close}
         </article>
 
